7538f921e22084d1ad9ed9a34e79016d24b56615
Steffen Karlsson <steffen.karlsson@gmail.com>
2025-09-29 16:16:35 +0200

    chore(controller): separated logic for controllers and better logic and scheme
    Signed-off-by: Steffen Karlsson <steffen.karlsson@gmail.com>

--- pkg/controller/scaleway.go
+++ pkg/controller/scaleway.go
@@ -4,9 +4,30 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/kommodity-io/kommodity/pkg/logging"
 	scaleway_capi_controller "github.com/scaleway/cluster-api-provider-scaleway/pkg/controller"
 	ctrl "sigs.k8s.io/controller-runtime"
 )
+
+func setupScaleway(ctx context.Context, manager ctrl.Manager) error {
+	logger := logging.FromContext(ctx)
+
+	logger.Info("Setting up ScalewayCluster controller")
+
+	err := setupScalewayClusterWithManager(ctx, manager)
+	if err != nil {
+		return fmt.Errorf("failed to setup ScalewayCluster controller: %w", err)
+	}
+
+	logger.Info("Setting up ScalewayMachine controller")
+
+	err = setupScalewayMachineWithManager(ctx, manager)
+	if err != nil {
+		return fmt.Errorf("failed to setup ScalewayMachine controller: %w", err)
+	}
+
+	return nil
+}
 
 func setupScalewayMachineWithManager(_ context.Context, manager ctrl.Manager) error {
 	err := scaleway_capi_controller.NewScalewayMachineReconciler(manager.GetClient()).SetupWithManager(manager)
--- pkg/controller/talos.go
+++ pkg/controller/talos.go
@@ -12,6 +12,27 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 )
 
+func setupTalos(ctx context.Context, manager ctrl.Manager,
+	maxConcurrentReconciles int) error {
+	logger := logging.FromContext(ctx)
+
+	logger.Info("Setting up TalosConfig controller")
+
+	err := setupTalosConfigWithManager(ctx, manager, maxConcurrentReconciles)
+	if err != nil {
+		return fmt.Errorf("failed to setup TalosConfig controller: %w", err)
+	}
+
+	logger.Info("Setting up TalosControlPlane controller")
+
+	err = setupTalosControlPlaneWithManager(ctx, manager, maxConcurrentReconciles)
+	if err != nil {
+		return fmt.Errorf("failed to setup TalosControlPlane controller: %w", err)
+	}
+
+	return nil
+}
+
 func setupTalosConfigWithManager(ctx context.Context, manager ctrl.Manager,
 	maxConcurrentReconciles int) error {
 	err := (&bootstrap_controller.TalosConfigReconciler{
