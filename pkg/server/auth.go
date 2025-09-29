64d4f0732340421cb81c3b8d3e06ae6421687472
Steffen Karlsson <steffen.karlsson@gmail.com>
2025-09-29 07:52:49 +0200

    feat(auth): adding admin authorizer with system:masters bypass
    Signed-off-by: Steffen Karlsson <steffen.karlsson@gmail.com>

--- pkg/server/auth.go
+++ pkg/server/auth.go
@@ -3,16 +3,49 @@ package server
 import (
 	"context"
 	"fmt"
+	"slices"
 
 	kommodityconfig "github.com/kommodity-io/kommodity/pkg/config"
 	"k8s.io/apiserver/pkg/apis/apiserver"
 	"k8s.io/apiserver/pkg/authentication/authenticator"
 	bearertoken "k8s.io/apiserver/pkg/authentication/request/bearertoken"
 	authunion "k8s.io/apiserver/pkg/authentication/request/union"
-	"k8s.io/apiserver/pkg/authorization/authorizerfactory"
+	"k8s.io/apiserver/pkg/authorization/authorizer"
+	auth "k8s.io/apiserver/pkg/authorization/authorizer"
 	genericapiserver "k8s.io/apiserver/pkg/server"
 	oidc "k8s.io/apiserver/plugin/pkg/authenticator/token/oidc"
 )
+
+const (
+	systemPrivilegedGroup = "system:masters"
+)
+
+type adminAuthorizer struct{}
+
+func (a *adminAuthorizer) Authorize(_ context.Context, attrs auth.Attributes) (auth.Decision, string, error) {
+	user := attrs.GetUser()
+	if user == nil {
+		// no user — probably unauthenticated
+		return authorizer.DecisionDeny, "no user in attributes", nil
+	}
+
+	adminGroup, err := kommodityconfig.GetAdminGroup()
+	if err != nil {
+		return auth.DecisionDeny,
+			"forbidden: no admin group configured",
+			fmt.Errorf("no admin group configured: %w", err)
+	}
+
+	if slices.Contains(user.GetGroups(), systemPrivilegedGroup) {
+		return auth.DecisionAllow, "allowed: user is in system:masters group", nil
+	}
+
+	if slices.Contains(user.GetGroups(), adminGroup) {
+		return auth.DecisionAllow, "allowed: user is in admin group", nil
+	}
+
+	return auth.DecisionDeny, "forbidden: user is not in admin group or system:masters group", nil
+}
 
 func applyAuth(ctx context.Context, config *genericapiserver.RecommendedConfig) error {
 	if !kommodityconfig.ApplyAuth(ctx) {
@@ -59,7 +92,7 @@ func applyAuth(ctx context.Context, config *genericapiserver.RecommendedConfig) 
 
 	bearerOIDC := bearertoken.New(oidcAuth)
 
-	config.Authorization.Authorizer = authorizerfactory.NewAlwaysAllowAuthorizer()
+	config.Authorization.Authorizer = &adminAuthorizer{}
 
 	config.Authentication.APIAudiences = authenticator.Audiences(jwtAuthenticator.Issuer.Audiences)
 	config.Authentication.Authenticator = authunion.New(bearerOIDC)
