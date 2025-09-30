064184c7b40ad3c7a1922996cd57f303769c6c37
Steffen Karlsson <steffen.karlsson@gmail.com>
2025-09-30 11:35:23 +0200

    feat(provider): adding provider cache for better decoupling of hooks and fail fast startup
    Signed-off-by: Steffen Karlsson <steffen.karlsson@gmail.com>

--- pkg/server/errors.go
+++ pkg/server/errors.go
@@ -8,4 +8,8 @@ var (
 	ErrInvalidKommodityDebugVar = errors.New("KOMMODITY_DEBUG environment variable not a valid bool format")
 	// ErrFailedToAppendCAData indicates that appending the CA certificate failed.
 	ErrFailedToAppendCAData = errors.New("failed to append CA cert")
+	// ErrMissingOIDCConfig indicates that the OIDC configuration is not set.
+	ErrMissingOIDCConfig = errors.New("OIDC configuration is not set")
+	// ErrTimeoutWaitingForCRD indicates that the timeout waiting for CRD to be established.
+	ErrTimeoutWaitingForCRD = errors.New("timeout waiting for CRD to be established")
 )
