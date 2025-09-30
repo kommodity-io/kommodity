dacf0e71857aefe7595103534650c738cc3aa996
Steffen Karlsson <steffen.karlsson@gmail.com>
2025-09-30 11:47:42 +0200

    feat(provider): adding provider cache for better decoupling of hooks and fail fast startup (#79)
    Signed-off-by: Steffen Karlsson <steffen.karlsson@gmail.com>

--- pkg/server/erros.go
+++
@@ -1,9 +0,0 @@
-// Package server provides error definitions for server operations.
-package server
-
-import "errors"
-
-var (
-	// ErrMissingOIDCConfig indicates that the OIDC configuration is not set.
-	ErrMissingOIDCConfig = errors.New("OIDC configuration is not set")
-)
