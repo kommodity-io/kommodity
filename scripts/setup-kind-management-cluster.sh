923597a6581a6049e2bab79940312f642fa93352
Paul Thuriot <pth@corti.ai>
2025-10-02 12:56:07 +0200

    feat(development): added script to setup a capi management cluster (#88)
    Signed-off-by: Paul Thuriot <pth@corti.ai>
    Signed-off-by: lucas-tremaroli <lucasluigitremaroli@outlook.com>

---
+++ scripts/setup-kind-management-cluster.sh
@@ -0,0 +1,24 @@
+# This script sets up a kind management cluster
+# It is meant to be used for debugging and testing purposes, so we can directly compare the behavior of kommodity
+# against the behavior of CAPI 
+
+# The following providers will be installed: 
+#   - cluster-api core provider
+#   - talos control plane provider
+#   - talos bootstrap provider
+#   - scaleway infrastructure provider
+
+# Prerequisites:
+#   - kind installed (https://kind.sigs.k8s.io/docs/user/quick-start/)
+#   - clusterctl installed (https://cluster-api.sigs.k8s.io/user/quick-start#install-clusterctl)
+
+set -euo pipefail
+
+echo "🚀 Setting up kind management cluster..."
+kind create cluster --name kind-management
+
+# Initialize the management cluster
+echo "🔧 Initializing cluster with CAPI, Talos, and Scaleway providers..."
+clusterctl init --infrastructure scaleway --control-plane talos --bootstrap talos
+
+echo "✅ Management cluster setup complete."
