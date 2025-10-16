75ac537addfc5c2dbf4297df50fe8ae94b07df86
Steffen Karlsson <steffen.karlsson@gmail.com>
2025-10-16 18:00:16 +0200

    fix(extensionserver): wire down the AdmissionControl from API server
    Signed-off-by: Steffen Karlsson <steffen.karlsson@gmail.com>

--- pkg/server/apiextensionsserver.go
+++ pkg/server/apiextensionsserver.go
@@ -86,6 +86,7 @@ func setupAPIExtensionConfig(cfg *config.KommodityConfig,
 	crdRecommended.MergedResourceConfig = genericServerConfig.MergedResourceConfig
 	crdRecommended.BuildHandlerChainFunc = genericapiserver.BuildHandlerChainWithStorageVersionPrecondition
 	crdRecommended.SharedInformerFactory = genericServerConfig.SharedInformerFactory
+	crdRecommended.AdmissionControl = genericServerConfig.AdmissionControl
 
 	return &apiextensionsapiserver.Config{
 		GenericConfig: crdRecommended,
