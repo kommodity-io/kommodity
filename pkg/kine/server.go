b0800a718188668cb84dc63736883ddd7889bec1
Steffen Karlsson <steffen.karlsson@gmail.com>
2025-10-23 14:31:24 +0200

    fix(kine): add wait for Kine to fix race condition (#112)
    Signed-off-by: Steffen Karlsson <steffen.karlsson@gmail.com>

--- pkg/kine/server.go
+++ pkg/kine/server.go
@@ -1,20 +1,68 @@
 package kine
 
 import (
+	"context"
 	"fmt"
+	"net"
+	"time"
 
 	kineconfig "github.com/k3s-io/kine/pkg/app"
 	"github.com/kommodity-io/kommodity/pkg/config"
+	"github.com/kommodity-io/kommodity/pkg/logging"
 )
 
+const (
+	kineDialTimeout = 2 * time.Second
+)
+
+// Server represents a Kine server instance.
+type Server struct {
+	cfg *config.KommodityConfig
+}
+
+// NewServer creates a new Kine server instance.
+func NewServer(cfg *config.KommodityConfig) *Server {
+	return &Server{
+		cfg: cfg,
+	}
+}
+
 // StartKine starts a Kine server based on the provided Kommodity configuration.
-func StartKine(cfg *config.KommodityConfig) error {
+func (ks *Server) StartKine() error {
 	kineApp := kineconfig.New()
 
-	err := kineApp.Run(append([]string{"kine"}, "--endpoint="+cfg.DBURI.String()))
+	err := kineApp.Run(append([]string{"kine"}, "--endpoint="+ks.cfg.DBURI.String()))
 	if err != nil {
 		return fmt.Errorf("failed to start kine: %w", err)
 	}
 
 	return nil
+}
+
+// WaitForKine waits until the Kine server is ready to accept TCP connections.
+func (ks *Server) WaitForKine(ctx context.Context, readyChan chan struct{}) {
+	go func() {
+		logger := logging.FromContext(ctx)
+		for {
+			logger.Info("Waiting for Kine to be ready...")
+
+			dialer := &net.Dialer{Timeout: kineDialTimeout}
+
+			conn, err := dialer.DialContext(
+				ctx,
+				"tcp",
+				ks.cfg.KineURI,
+			)
+			if err == nil {
+				// Intentionally ignore close errors
+				_ = conn.Close()
+
+				close(readyChan)
+
+				return
+			}
+
+			time.Sleep(kineDialTimeout)
+		}
+	}()
 }
