5a4fda2b199176421c634be7473a6f5ed4602869
Steffen Karlsson <steffen.karlsson@gmail.com>
2025-10-27 08:26:05 +0100

    Update pkg/attestation/errors.go
    Co-authored-by: Nicklas Frahm <nicklas.frahm@gmail.com>
    Signed-off-by: Steffen Karlsson <steffen.karlsson@gmail.com>

--- pkg/attestation/errors.go
+++ pkg/attestation/errors.go
@@ -4,10 +4,10 @@ package attestation
 import "errors"
 
 var (
-	// ErrInvalidNounce is returned when the nonce is invalid.
-	ErrInvalidNounce = errors.New("invalid nonce")
-	// ErrExpiredNounce is returned when the nonce is expired.
-	ErrExpiredNounce = errors.New("expired nonce")
+	// ErrInvalidNonce is returned when the nonce is invalid.
+	ErrInvalidNonce = errors.New("invalid nonce")
+	// ErrExpiredNonce is returned when the nonce is expired.
+	ErrExpiredNonce = errors.New("expired nonce")
 	// ErrNoMachineFound is returned when no machine is found for the given criteria.
 	ErrNoMachineFound = errors.New("no machine found")
 )
