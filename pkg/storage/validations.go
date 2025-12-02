127bbac013f9904b43e097a0dff92609ba96f27b
Steffen Karlsson <steffen.karlsson@gmail.com>
2025-12-02 09:59:24 +0100

    fix(predicate): move to separate file
    Signed-off-by: Steffen Karlsson <steffen.karlsson@gmail.com>

--- pkg/storage/validations.go
+++ pkg/storage/validations.go
@@ -3,12 +3,6 @@ package storage
 
 import (
 	"fmt"
-
-	"k8s.io/apimachinery/pkg/fields"
-	"k8s.io/apimachinery/pkg/labels"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apiserver/pkg/storage"
-	apistorage "k8s.io/apiserver/pkg/storage"
 )
 
 // FieldIsNonNull validates that the given value is not nil.
@@ -19,28 +13,4 @@ func FieldIsNonNull(field string, _ bool) []string {
 	}
 
 	return allErrs
-}
-
-// PredicateFunc returns a selection predicate function for the given getAttrs function.
-//
-//nolint:lll // Cannot be broken into multiple lines.
-func PredicateFunc(getAttrs func(obj runtime.Object) (labels.Set, fields.Set, error)) func(label labels.Selector, field fields.Selector) apistorage.SelectionPredicate {
-	return func(label labels.Selector, field fields.Selector) apistorage.SelectionPredicate {
-		return apistorage.SelectionPredicate{
-			Label:    label,
-			Field:    field,
-			GetAttrs: getAttrs,
-		}
-	}
-}
-
-// NamespacedPredicateFunc returns a selection predicate function.
-func NamespacedPredicateFunc() func(label labels.Selector, field fields.Selector) apistorage.SelectionPredicate {
-	return func(label labels.Selector, field fields.Selector) apistorage.SelectionPredicate {
-		return apistorage.SelectionPredicate{
-			Label:    label,
-			Field:    field,
-			GetAttrs: storage.DefaultNamespaceScopedAttr,
-		}
-	}
 }
