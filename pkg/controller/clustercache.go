aa47a263269b3658682a7ea0d82affc0000289df
Steffen Karlsson <steffen.karlsson@gmail.com>
2025-12-03 13:16:09 +0100

    fix(core): adding index for provider id to fix missing NodeRef (#183)
    Signed-off-by: Steffen Karlsson <steffen.karlsson@gmail.com>

--- pkg/controller/clustercache.go
+++ pkg/controller/clustercache.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"sigs.k8s.io/cluster-api/api/v1beta1/index"
 	"sigs.k8s.io/cluster-api/controllers/clustercache"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
@@ -11,8 +12,16 @@ import (
 
 func setupClusterCacheWithManager(ctx context.Context, manager ctrl.Manager,
 	opt controller.Options) (clustercache.ClusterCache, error) {
+	err := index.AddDefaultIndexes(ctx, manager)
+	if err != nil {
+		return nil, fmt.Errorf("failed to add default indexes: %w", err)
+	}
+
 	cache, err := clustercache.SetupWithManager(ctx, manager, clustercache.Options{
 		SecretClient: manager.GetClient(),
+		Cache: clustercache.CacheOptions{
+			Indexes: []clustercache.CacheOptionsIndex{clustercache.NodeProviderIDIndex},
+		},
 		Client: clustercache.ClientOptions{
 			UserAgent: "kommodity-clustercache",
 		},
