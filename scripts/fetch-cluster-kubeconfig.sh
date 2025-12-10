44a37276de8c7410154996027ae0bd96fb3b36ed
Paul Thuriot <pth@corti.ai>
2025-12-10 14:14:36 +0100

    feat(scripts): added useful scripts
    Signed-off-by: Paul Thuriot <pth@corti.ai>

---
+++ scripts/fetch-cluster-kubeconfig.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+CLUSTER_NAME=$1
+if [ -z "$CLUSTER_NAME" ]; then
+  echo "Usage: $0 <cluster-name> <kommodity-context>"
+  exit 1
+fi
+
+KOMMODITY_CONTEXT=$2
+if [ -z "$KOMMODITY_CONTEXT" ]; then
+  echo "Usage: $0 <cluster-name> <kommodity-context>"
+  exit 1
+fi
+
+TALOS_CONFIG_PATH=/tmp/talosconfig
+
+kubectl --context $KOMMODITY_CONTEXT get secrets ${CLUSTER_NAME}-talosconfig -ojson | jq -r '.data.talosconfig' | base64 -d > $TALOS_CONFIG_PATH
+
+node_IP=$(cat $TALOS_CONFIG_PATH | yq -r ".contexts.${CLUSTER_NAME}.endpoints[0]")
+
+echo "🔗 Updating kubeconfig with node IP: $node_IP"
+
+talosctl --talosconfig $TALOS_CONFIG_PATH kubeconfig -n $node_IP
+
+echo "✅ Kubeconfig updated successfully."
+
+rm -f $TALOS_CONFIG_PATH
