f26d5a8d3867c3b45da29a7fe17bdac3d9a3dbd0
Paul Thuriot <pth@corti.ai>
2025-12-11 10:16:47 +0100

    feat(terraform): output container app ID (#204)
    Signed-off-by: Paul Thuriot <pth@corti.ai>

--- terraform/modules/kommodity_azure_deployment/output.tf
+++ terraform/modules/kommodity_azure_deployment/output.tf
@@ -2,3 +2,8 @@ output "kommodity_app_url" {
   value       = "https://${azurerm_container_app.kommodity-app.ingress[0].fqdn}"
   description = "The URL of the Kommodity Container App"
 }
+
+output "kommodity_container_app_id" {
+  value       = azurerm_container_app.kommodity-app.id
+  description = "The ID of the Kommodity Container App"
+}
