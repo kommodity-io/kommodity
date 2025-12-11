4653de59ca448e91a6c6c0361ea4b4c208f41850
Paul Thuriot <pth@corti.ai>
2025-12-11 12:56:28 +0100

    feat(terraform): output container app's custom_domain_verification_id
    Signed-off-by: Paul Thuriot <pth@corti.ai>

--- terraform/modules/kommodity_azure_deployment/output.tf
+++ terraform/modules/kommodity_azure_deployment/output.tf
@@ -1,9 +1,14 @@
-output "kommodity_app_url" {
+output "app_url" {
   value       = "https://${azurerm_container_app.kommodity-app.ingress[0].fqdn}"
   description = "The URL of the Kommodity Container App"
 }
 
-output "kommodity_container_app_id" {
+output "container_app_id" {
   value       = azurerm_container_app.kommodity-app.id
   description = "The ID of the Kommodity Container App"
+}
+
+output "custom_domain_verification_id" {
+  value       = azurerm_container_app.kommodity-app.custom_domain_verification_id
+  description = "The Custom Domain Verification ID of the Kommodity Container App"
 }
