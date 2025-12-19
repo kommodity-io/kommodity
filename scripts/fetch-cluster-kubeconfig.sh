db0967da4579aa6bb8f16eb4c049842827e8638e
Paul Thuriot <pth@corti.ai>
2025-12-19 15:26:40 +0100

    feat(scripts): removed dangerous script
    Signed-off-by: Paul Thuriot <pth@corti.ai>

--- scripts/fetch-cluster-kubeconfig.sh
+++ scripts/fetch-cluster-kubeconfig.sh
@@ -3,19 +3,13 @@ set -e
 
 CLUSTER_NAME=$1
 if [ -z "$CLUSTER_NAME" ]; then
-  echo "Usage: $0 <cluster-name> <kommodity-context>"
-  exit 1
-fi
-
-KOMMODITY_CONTEXT=$2
-if [ -z "$KOMMODITY_CONTEXT" ]; then
-  echo "Usage: $0 <cluster-name> <kommodity-context>"
+  echo "Usage: $0 <cluster-name>"
   exit 1
 fi
 
 TALOS_CONFIG_PATH=/tmp/talosconfig
 
-kubectl --context "$KOMMODITY_CONTEXT" get secrets "${CLUSTER_NAME}-talosconfig" -ojson | jq -r '.data.talosconfig' | base64 -d > $TALOS_CONFIG_PATH
+kubectl get secrets "${CLUSTER_NAME}-talosconfig" -ojson | jq -r '.data.talosconfig' | base64 -d > $TALOS_CONFIG_PATH
 
 node_IP=$(yq -r ".contexts.\"${CLUSTER_NAME}\".endpoints[0]" "$TALOS_CONFIG_PATH")
 
