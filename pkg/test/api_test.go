c5e56249a4a219374f0eea8c61b5bbd693e4cb5b
Steffen Karlsson <steffen.karlsson@gmail.com>
2026-02-03 10:52:15 +0100

    fix(test): ensure that kommodity namespace exists in automated tests
    Signed-off-by: Steffen Karlsson <steffen.karlsson@gmail.com>

--- pkg/test/api_test.go
+++ pkg/test/api_test.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/kommodity-io/kommodity/pkg/test/helpers"
 	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go/internal/config"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -88,6 +89,16 @@ func TestCreateScalewayCluster(t *testing.T) {
 	_, err = client.CoreV1().Namespaces().Create(ctx, &corev1.Namespace{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: "default",
+		},
+	}, metav1.CreateOptions{})
+	if err != nil && !apierrors.IsAlreadyExists(err) {
+		require.NoError(t, err)
+	}
+
+	// Ensure kommodity namespace exists
+	_, err = client.CoreV1().Namespaces().Create(ctx, &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: config.KommodityNamespace,
 		},
 	}, metav1.CreateOptions{})
 	if err != nil && !apierrors.IsAlreadyExists(err) {
