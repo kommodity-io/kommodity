71b310b3abb4694db5bd172c5d5acdae21976f0f
Steffen Karlsson <steffen.karlsson@gmail.com>
2026-04-18 17:24:26 +0200

    fix(ui): pinned tailwind
    Signed-off-by: Steffen Karlsson <steffen.karlsson@gmail.com>

--- pkg/ui/server.go
+++ pkg/ui/server.go
@@ -3,12 +3,18 @@ package ui
 
 import (
 	"context"
+	"embed"
+	"fmt"
+	"io/fs"
 	"net/http"
 
 	"github.com/kommodity-io/kommodity/pkg/combinedserver"
 	"github.com/kommodity-io/kommodity/pkg/config"
 	"github.com/kommodity-io/kommodity/pkg/logging"
 )
+
+//go:embed public
+var publicFS embed.FS
 
 // NewHTTPMuxFactory creates a new HTTP mux factory for serving the Kommodity UI.
 func NewHTTPMuxFactory(
@@ -19,8 +25,13 @@ func NewHTTPMuxFactory(
 	logger.Info("Initializing Kommodity UI server")
 
 	return func(mux *http.ServeMux) error {
-		// Serve static files from public directory
-		mux.Handle("GET /public/", http.StripPrefix("/public/", http.FileServer(http.Dir("./public"))))
+		// Serve embedded UI assets
+		publicSubFS, err := fs.Sub(publicFS, "public")
+		if err != nil {
+			return fmt.Errorf("failed to create public filesystem: %w", err)
+		}
+
+		mux.Handle("GET /ui/public/", http.StripPrefix("/ui/public/", http.FileServer(http.FS(publicSubFS))))
 
 		// UI router with HTMX templates
 		router := NewRouter(cfg, logger)
